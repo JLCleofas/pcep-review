a4caa5ed5d7e732882074938d03f6f4fc9f03d61
JLCleofas <164037152+JLCleofas@users.noreply.github.com>
2025-06-11 16:57:19 +0800

    Day 1 activities

---
+++ pcep-prepweek1/hello_world.py
@@ -0,0 +1,16 @@
+#June 11, 2025
+#This is the start of my review for PCEP.
+#Sana di ako tamarin.
+print("Hello, World!")
+
+name = "John"
+
+print(name)
+
+
+'''Multiline comment example
+pwede pala 'to...'''
+
+full_name = "Juan Cruz"
+
+print(f"Hello {full_name}")
