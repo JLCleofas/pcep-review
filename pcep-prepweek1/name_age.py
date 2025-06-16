ded0e05e9340abc62b7ac1f5534ac7a1df6c1dfa
JLCleofas <164037152+JLCleofas@users.noreply.github.com>
2025-06-16 14:39:11 +0800

    Added name_age.py

--- pcep-prepweek1/name_age.py
+++ pcep-prepweek1/name_age.py
@@ -1,8 +1,5 @@
-num1 = 5
-print(num1, 'is of type', type(num1))
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
 
-num2 = 2.0
-print(num2, 'is of type', type(num2))
-
-num3 = 1+2j
-print(num3, 'is of type', type(num3))
+print(f"Hi {name}!")
+print(f"You were born on {2025 - age}")
